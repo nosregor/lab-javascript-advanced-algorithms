5e1a458acd3f7abec4976e7f5512b8c56aca2653
Andrew Rogerson <werdnanosregor@gmail.com>
2018-10-18 09:34:22 +0200

    Done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,39 @@
-function QueueDataStructure () {
+class QueueDataStructure {
+  constructor() {
+    this.queueControl = []
+    this.MAX_SIZE = 10
+  };
+  isEmpty() {
+    if (this.queueControl.length > 0) return false;
+    return true
+  };
+  canEnqueue() {
+    if (this.queueControl.length < this.MAX_SIZE) return true;
+    return false
+  };
+  enqueue(el) {
+    if (this.canEnqueue()) {
+      this.queueControl.unshift(el)
+    } else {
+      return 'Queue Overflow'
+    }
+    return this.queueControl
+  };
+  dequeue() {
+    if (this.isEmpty()) {
+      return "Queue Underflow"
+    }
+    let firstElement = this.queueControl.shift()
+    return firstElement
+  };
 }
+
+
+// stack = new QueueDataStructure()
+// stack.enqueue(19);
+// stack.enqueue(88);
+// console.log(stack.queueControl)
+
+// // stack.dequeue();
+// console.log(stack.dequeue())
+// // console.log(stack)
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,36 @@
-function StackDataStructure () {
+class StackDataStructure {
+  constructor() {
+    this.stackControl = []
+    this.MAX_SIZE = 10
+  }
+  isEmpty() {
+    if (this.stackControl.length > 0) return false;
+    return true
+  };
+  canPush() {
+    if (this.stackControl.length < this.MAX_SIZE) return true;
+    return false
+  };
+  push(el) {
+    if (this.canPush()) {
+      this.stackControl.push(el)
+    } else {
+      return 'Stack Overflow'
+    }
+    return this.stackControl
+  };
+  pop() {
+    if (this.isEmpty()) {
+      return "Stack Underflow"
+    }
+    let lastElement = this.stackControl.pop()
+    return lastElement
+  };
 }
+// stack = new StackDataStructure();
+// stack.push(19);
+// stack.push(88);
+// console.log(stack.stackControl)
+
+
+// console.log(stack.pop())
